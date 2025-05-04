43a8a5d3ce19d28b37dd240635ac7d5ad1dd8266
Henrique Hida <hmhida@icloud.com>
2025-05-04 18:08:49 -0300

    first commit

---
+++ src/main/java/com/cohida/JudokaRegister/JudokaRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.cohida.JudokaRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudokaRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudokaRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cohida/JudokaRegister/JudokaRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohida.JudokaRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudokaRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

eedb64b6d278e835a03394a704316acb484c6af6
Henrique Hida <hmhida@icloud.com>
2025-05-04 18:47:59 -0300

    Criado a controller [2]

--- src/main/java/com/cohida/JudokaRegister/Controller.java
+++ src/main/java/com/cohida/JudokaRegister/Controller.java
@@ -1,4 +1,16 @@
 package com.cohida.JudokaRegister;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Bem-vindo!";
+    }
+
 }

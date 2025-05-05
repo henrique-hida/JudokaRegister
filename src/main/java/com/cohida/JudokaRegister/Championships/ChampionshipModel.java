318adddf8cb98b4b8d983375106b403c8bf077cb
Henrique Hida <hmhida@icloud.com>
2025-05-04 23:04:58 -0300

    Lombok

--- src/main/java/com/cohida/JudokaRegister/Championships/ChampionshipModel.java
+++ src/main/java/com/cohida/JudokaRegister/Championships/ChampionshipModel.java
@@ -3,11 +3,17 @@ package com.cohida.JudokaRegister.Championships;
 import com.cohida.JudokaRegister.Enums.Countries;
 import com.cohida.JudokaRegister.Judokas.JudokaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_championships")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChampionshipModel {
 
     @Id
--- src/main/java/com/cohida/JudokaRegister/Judokas/JudokaModel.java
+++ src/main/java/com/cohida/JudokaRegister/Judokas/JudokaModel.java
@@ -4,11 +4,15 @@ import com.cohida.JudokaRegister.Championships.ChampionshipModel;
 import com.cohida.JudokaRegister.Enums.Countries;
 import com.cohida.JudokaRegister.Enums.Obis;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_judokas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JudokaModel {
 
     @Id
@@ -25,55 +29,6 @@ public class JudokaModel {
     @JoinColumn(name = "championship_id") // FK
     private ChampionshipModel championship;
 
-    public JudokaModel() {
-    }
 
-    public JudokaModel(String name, float weight, int age, Countries country, Obis belt) {
-        this.name = name;
-        this.weight = weight;
-        this.age = age;
-        this.country = country;
-        this.belt = belt;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Countries getCountry() {
-        return country;
-    }
-
-    public void setCountry(Countries country) {
-        this.country = country;
-    }
-
-    public Obis getBelt() {
-        return belt;
-    }
-
-    public void setBelt(Obis belt) {
-        this.belt = belt;
-    }
 }
 

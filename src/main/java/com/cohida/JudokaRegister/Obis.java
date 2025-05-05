d9d9d756ee7c23955962cb65209bc50efb0a7c14
Henrique Hida <hmhida@icloud.com>
2025-05-04 21:39:54 -0300

    reconfigurando main [2]

--- src/main/java/com/cohida/JudokaRegister/Obis.java
+++ src/main/java/com/cohida/JudokaRegister/Obis.java
@@ -1,4 +1,15 @@
 package com.cohida.JudokaRegister;
 
 public enum Obis {
+    WHITE,
+    GRAY,
+    BLUE,
+    YELLOW,
+    ORANGE,
+    GREEN,
+    PURPLE,
+    BROWN,
+    BLACK,
+    CORAL,
+    RED;
 }

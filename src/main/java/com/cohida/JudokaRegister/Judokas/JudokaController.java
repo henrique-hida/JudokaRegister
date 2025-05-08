2302c8fa3ece6315b61e9ced3c721ab81435091d
Henrique Hida <hmhida@icloud.com>
2025-05-08 00:48:48 -0300

    feat: create fake endpoints (data mock)

--- src/main/java/com/cohida/JudokaRegister/Judokas/JudokaController.java
+++ src/main/java/com/cohida/JudokaRegister/Judokas/JudokaController.java
@@ -1,8 +1,6 @@
 package com.cohida.JudokaRegister.Judokas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class JudokaController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Bem-vindo!";
+    }
+
+    // Add judoka (CREATE)
+    @PostMapping("/newjudoka")
+    public String newJudoka() {
+        return "Judoka criado";
+    }
+
+    // Show all judokas (READ)
+    @GetMapping("/judokaslist")
+    public String showAllJudokas() {
+        return "Todos os judokas";
+    }
+
+    // Find judoka by (READ)
+    @GetMapping("/judokaID")
+    public String showAllJudokasById() {
+        return "Judoka por ID";
+    }
+
+    // Alter judoka data (UPDATE)
+    @PutMapping("/alterID")
+    public String alterJudoka() {
+        return "Judoka alterado";
+    }
+
+    // Delete judoka (DELETE)
+    @DeleteMapping("/deleteID")
+    public String deleteJudoka() {
+        return "Judoka deletado";
     }
 
 }

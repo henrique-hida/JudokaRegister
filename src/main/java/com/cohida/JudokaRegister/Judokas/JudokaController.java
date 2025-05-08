0cb014f2e46ef9a92d2d7c8944d132311cad9150
Henrique Hida <hmhida@icloud.com>
2025-05-08 01:06:12 -0300

    fix: change judokas endpoints names

--- src/main/java/com/cohida/JudokaRegister/Judokas/JudokaController.java
+++ src/main/java/com/cohida/JudokaRegister/Judokas/JudokaController.java
@@ -3,40 +3,35 @@ package com.cohida.JudokaRegister.Judokas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("judokas")
 public class JudokaController {
 
-    @GetMapping("/welcome")
-    public String welcome() {
-        return "Bem-vindo!";
-    }
-
-    // Add judoka (CREATE)
-    @PostMapping("/newjudoka")
-    public String newJudoka() {
-        return "Judoka criado";
-    }
-
     // Show all judokas (READ)
-    @GetMapping("/judokaslist")
+    @GetMapping("/list")
     public String showAllJudokas() {
         return "Todos os judokas";
     }
 
     // Find judoka by (READ)
-    @GetMapping("/judokaID")
+    @GetMapping("/listID")
     public String showAllJudokasById() {
         return "Judoka por ID";
     }
 
+    // Add judoka (CREATE)
+    @PostMapping("/create")
+    public String newJudoka() {
+        return "Judoka criado";
+    }
+
     // Alter judoka data (UPDATE)
-    @PutMapping("/alterID")
+    @PutMapping("/change")
     public String alterJudoka() {
         return "Judoka alterado";
     }
 
     // Delete judoka (DELETE)
-    @DeleteMapping("/deleteID")
+    @DeleteMapping("/delete")
     public String deleteJudoka() {
         return "Judoka deletado";
     }

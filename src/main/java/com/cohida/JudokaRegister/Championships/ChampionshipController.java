021547929d4420c5b324294abe06b1f0490719bb
Henrique Hida <hmhida@icloud.com>
2025-05-08 01:02:14 -0300

    feat: create fake endpoints to championships (data mock)

--- src/main/java/com/cohida/JudokaRegister/Championships/ChampionshipController.java
+++ src/main/java/com/cohida/JudokaRegister/Championships/ChampionshipController.java
@@ -1,4 +1,29 @@
 package com.cohida.JudokaRegister.Championships;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("championships")
 public class ChampionshipController {
+
+    @GetMapping("/list")
+    public String listChampionships() {
+        return "Championship showed";
+    }
+
+    @PostMapping("/create")
+    public String createChampionship() {
+        return "Championship created succesfully";
+    }
+
+    @PutMapping("/change")
+    public String changeChampionship() {
+        return "Championship changed succesfully";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteChampionship() {
+        return "Championship deleted succesfully";
+    }
+
 }

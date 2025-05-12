e7199dfb886099c1a307e82df279b9f28101bb7e
Henrique Hida <hmhida@icloud.com>
2025-05-12 00:13:57 -0300

    chore: include DTO version by entity and manually map the paths entity x dto in JudokaMapper

--- src/main/java/com/cohida/JudokaRegister/Judokas/JudokaDTO.java
+++ src/main/java/com/cohida/JudokaRegister/Judokas/JudokaDTO.java
@@ -1,4 +1,23 @@
 package com.cohida.JudokaRegister.Judokas;
+import com.cohida.JudokaRegister.Championships.ChampionshipModel;
+import com.cohida.JudokaRegister.Enums.Countries;
+import com.cohida.JudokaRegister.Enums.Obis;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JudokaDTO {
+
+    private Long id;
+    private String name;
+    private float weight;
+    private int age;
+    private Countries country;
+    private Obis belt;
+    private String rank;
+    private ChampionshipModel championship;
+
 }
--- src/main/java/com/cohida/JudokaRegister/Judokas/JudokaMapper.java
+++ src/main/java/com/cohida/JudokaRegister/Judokas/JudokaMapper.java
@@ -1,4 +1,36 @@
 package com.cohida.JudokaRegister.Judokas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JudokaMapper {
+
+    public JudokaModel map(JudokaDTO judokaDTO) {
+        JudokaModel judokaModel = new JudokaModel();
+        judokaModel.setId(judokaDTO.getId());
+        judokaModel.setName(judokaDTO.getName());
+        judokaModel.setWeight(judokaDTO.getWeight());
+        judokaModel.setAge(judokaDTO.getAge());
+        judokaModel.setCountry(judokaDTO.getCountry());
+        judokaModel.setBelt(judokaDTO.getBelt());
+        judokaModel.setRank(judokaDTO.getRank());
+        judokaModel.setChampionship(judokaDTO.getChampionship());
+
+        return judokaModel;
+    }
+
+    public JudokaDTO map(JudokaModel judokaModel) {
+        JudokaDTO judokaDTO = new JudokaDTO();
+        judokaDTO.setId(judokaModel.getId());
+        judokaDTO.setName(judokaModel.getName());
+        judokaDTO.setWeight(judokaModel.getWeight());
+        judokaDTO.setAge(judokaModel.getAge());
+        judokaDTO.setCountry(judokaModel.getCountry());
+        judokaDTO.setBelt(judokaModel.getBelt());
+        judokaDTO.setRank(judokaModel.getRank());
+        judokaDTO.setChampionship(judokaModel.getChampionship());
+
+        return judokaDTO;
+    }
+
 }

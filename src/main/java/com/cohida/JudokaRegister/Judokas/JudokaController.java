c576d48e008c9317505b72bcee7b629750b32521
Henrique Hida <hmhida@icloud.com>
2025-05-14 01:23:13 -0300

    doc: add Swagger to application to document all routes

--- src/main/java/com/cohida/JudokaRegister/Judokas/JudokaController.java
+++ src/main/java/com/cohida/JudokaRegister/Judokas/JudokaController.java
@@ -1,4 +1,8 @@
 package com.cohida.JudokaRegister.Judokas;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,10 @@ public class JudokaController {
 
     // Show all judokas (READ)
     @GetMapping("/list")
+    @Operation(summary = "List all judokas", description = "List all judokas from db")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "Judoka found successfully")
+    })
     public ResponseEntity<List<JudokaDTO>> showAllJudokas() {
         List<JudokaDTO> judokas = judokaService.showAllJudokas();
         return ResponseEntity.ok(judokas);
@@ -23,6 +31,11 @@ public class JudokaController {
 
     // Find judoka by ID (READ)
     @GetMapping("/list/{id}")
+    @Operation(summary = "List a judoka by id", description = "List a judoka by its id")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "Judoka found successfully"),
+            @ApiResponse(responseCode = "404", description = "Judoka not found")
+    })
     public ResponseEntity<?> showJudokaById(@PathVariable Long id) {
         JudokaDTO judokaById = judokaService.showJudokaByID(id);
         if (judokaById != null) {
@@ -35,6 +48,11 @@ public class JudokaController {
 
     // Add judoka (CREATE)
     @PostMapping("/create")
+    @Operation(summary = "Create a new judoka", description = "Create a new judoka and insert into the db")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "201", description = "Judoka created successfully"),
+            @ApiResponse(responseCode = "400", description = "Error in judoka creation")
+    })
     public ResponseEntity<String> newJudoka(@RequestBody JudokaDTO judoka) {
         JudokaDTO newJudoka = judokaService.newJudoka(judoka);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -43,7 +61,16 @@ public class JudokaController {
 
     // Update judoka data (UPDATE)
     @PutMapping("/change/{id}")
-    public ResponseEntity<?> updateJudoka(@PathVariable Long id, @RequestBody JudokaDTO judokaUpdated) {
+    @Operation(summary = "Change judoka by id", description = "Edit judoka by id")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "Judoka updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Judoka not found")
+    })
+    public ResponseEntity<?> updateJudoka(
+            @Parameter(description = "user sends id in the request path")
+            @PathVariable Long id,
+            @Parameter(description = "user sends judoka data in the request path")
+            @RequestBody JudokaDTO judokaUpdated) {
         JudokaDTO updatedJudoka = judokaService.updateJudoka(id, judokaUpdated);
         if (updatedJudoka != null) {
             return ResponseEntity.ok(updatedJudoka);
@@ -56,6 +83,7 @@ public class JudokaController {
 
     // Delete judoka (DELETE)
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete judoka by id", description = "Delete judoka by id")
     public ResponseEntity<String> deleteJudoka(@PathVariable Long id) {
         if (judokaService.showJudokaByID(id) != null) {
             judokaService.deleteJudoka(id);
